2d19c0785243959b271cda71142a8882382ede6b
fadilahyudapratama <btufadilahyudapratama@gmail.com>
2024-02-01 08:32:33 +0700

    update accourdion && finising translate

--- src/pages/accordionTopupPage/AccordionTopUp.jsx
+++ src/pages/accordionTopupPage/AccordionTopUp.jsx
@@ -10,18 +10,32 @@ import IcoAccountBalance from "../../assets/icons/ico_account_balance.png";
 import IcoJakoneMobile from "../../assets/icons/ico_jakone_mobile.png";
 import IconCc2 from "../../assets/icons/icon_cc_2.png";
 
+import {
+  getDescriptionHeaderTopUpWithATMBankDki,
+  getBodyDescriptionTopUpWithATMBankDki,
+  getDescriptionTopUpWithATMBankDki,
+  getDescriptionHeaderTopUpWithATMBankLain,
+  getBodyDescriptionTopUpWithATMBankLain,
+  getDescriptionTopUpWithATMBankLain,
+  getDescriptionHeaderTopUpWithATMJakOneMobile,
+  getBodyDescriptionTopUpWithATMJakOneMobile,
+  getDescriptionTopUpWithATMJakOneMobile,
+  getDescriptionHeaderTopUpWithATMDebitKredit,
+  getBodyDescriptionTopUpWithATMDebitKredit,
+  getDescriptionTopUpWithATMDebitKredit,
+  getHeaderPageHowToTopUpWithJakonePay,
+} from "../../constantFile/I_Constant";
+
 function AccordionTopUp() {
   const [open, setOpen] = React.useState(0);
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
   return (
     <div>
-
       <div className="text-2xl my-8 mx-4 text-center">
-        <h1>
-          <strong>Topup JakOne Pay</strong>
-        </h1>
+        <h1>{getHeaderPageHowToTopUpWithJakonePay()}</h1>
       </div>
+
       <div className="p-7">
         <Accordion
           open={open === 1}
@@ -29,22 +43,22 @@ function AccordionTopUp() {
         >
           <AccordionHeader
             onClick={() => handleOpen(1)}
-            className={`border-b-0 transition-colors ${open === 1 ? "text-blue-500 hover:!text-blue-700" : ""
-              }`}
+            className={`border-b-0 transition-colors ${
+              open === 1 ? "text-blue-500 hover:!text-blue-700" : ""
+            }`}
           >
             <div className="grid grid-cols-6">
               <div className="...">
-                <img className="" width={60} src={IcoBankCard} alt="My Image" />
+                <img className="" width={30} src={IcoBankCard} alt="My Image" />
               </div>
               <div className="col-span-4 ml-4">
-                <strong>ATM Bank DKI</strong> <br />
-                <small>ATM Bank DKI terdekat</small>
+                {getDescriptionHeaderTopUpWithATMBankDki()}
               </div>
               <div className="relative w-32 ...">
                 <div className="absolute inset-x-0 top-0 h-16 ...">
                   <img
                     className="rotate-90"
-                    width={60}
+                    width={30}
                     src={IconChevronRight}
                     alt="My Image"
                   />
@@ -54,72 +68,12 @@ function AccordionTopUp() {
           </AccordionHeader>
           <AccordionBody className="">
             <h1 className="text-center font-semibold mb-2">
-              <strong>TopUp JakOne Pay</strong>
+              {getHeaderPageHowToTopUpWithJakonePay()}
             </h1>
-            <p>
-              Sekarang kamu dapat mengisi JakonePay melalui seluruh mesin ATM Bank
-              BKI. Ikuti Langkah-langkah di bawah ini untuk melakukan TopUp
-              JakOnePay
-            </p>
-            <div className="grid grid-cols-6 my-4">
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                1
-              </div>
-              <div className="col-span-4">
-                Pilih Menu <span>Pembayaran</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                2
-              </div>
-              <div className="col-span-4">
-                Pilih Menu
-                <span>Virtual Account</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                3
-              </div>
-              <div className="col-span-4">
-                Masukan <span>No. Virtual Account (9980) </span>
-                yang diikuti dengan nomor Handphone Kamu.
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                4
-              </div>
-              <div className="col-span-4">
-                kosongkan
-                <span>Kode Pembayaran</span>
-                lalu pilih <span>Lanjut</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                5
-              </div>
-              <div className="col-span-4">
-                Masukan
-                <span>Nominal Transaksi, </span>
-                lalu pilih <span>Lanjut</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                6
-              </div>
-              <div className="col-span-4">
-                Jika
-                <span>Nominal Transaksi, </span>
-                dan <span>Nomor Virtual Account </span>
-                benar, pilih Benar
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                7
-              </div>
-              <div className="col-span-4">
-                Setelah berhasil saldo JakOnePay Kamu akan bertambah
-              </div>
-            </div>
+
+            {getBodyDescriptionTopUpWithATMBankDki()}
+
+            {getDescriptionTopUpWithATMBankDki()}
           </AccordionBody>
         </Accordion>
 
@@ -129,29 +83,27 @@ function AccordionTopUp() {
         >
           <AccordionHeader
             onClick={() => handleOpen(2)}
-            className={`border-b-0 transition-colors ${open === 2 ? "text-blue-500 hover:!text-blue-700" : ""
-              }`}
+            className={`border-b-0 transition-colors ${
+              open === 2 ? "text-blue-500 hover:!text-blue-700" : ""
+            }`}
           >
             <div className="grid grid-cols-6">
               <div className="...">
                 <img
                   className=""
-                  width={60}
+                  width={30}
                   src={IcoAccountBalance}
                   alt="My Image"
                 />
               </div>
               <div className="col-span-4 ml-4">
-                <strong>Bank Lain</strong> <br />
-                <small>
-                  Kapan pun dimana pun melalui chanel bank di indonesia
-                </small>
+                {getDescriptionHeaderTopUpWithATMBankLain()}
               </div>
               <div className="relative w-32 ...">
                 <div className="absolute inset-x-0 top-0 h-16 ...">
                   <img
                     className="rotate-90"
-                    width={60}
+                    width={30}
                     src={IconChevronRight}
                     alt="My Image"
                   />
@@ -161,71 +113,11 @@ function AccordionTopUp() {
           </AccordionHeader>
           <AccordionBody className="">
             <h1 className="text-center font-semibold mb-2">
-              <strong>TopUp JakOne Pay</strong>
+              {getHeaderPageHowToTopUpWithJakonePay()}
             </h1>
-            <p>
-              Kamu dapat mengisi JakOne Pay melalui transfer melalui bank lain.
-              Ikuti Langkah-langkah dibawah ini untuk melakukan TopUp JakOne Pay.
-            </p>
-            <div className="grid grid-cols-6 my-4">
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                1
-              </div>
-              <div className="col-span-4">
-                Pilih Menu <span>Pembayaran</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                2
-              </div>
-              <div className="col-span-4">
-                Pilih Menu
-                <span>Virtual Account</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                3
-              </div>
-              <div className="col-span-4">
-                Masukan <span>No. Virtual Account (9980) </span>
-                yang diikuti dengan nomor Handphone Kamu.
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                4
-              </div>
-              <div className="col-span-4">
-                kosongkan
-                <span>Kode Pembayaran</span>
-                lalu pilih <span>Lanjut</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                5
-              </div>
-              <div className="col-span-4">
-                Masukan
-                <span>Nominal Transaksi, </span>
-                lalu pilih <span>Lanjut</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                6
-              </div>
-              <div className="col-span-4">
-                Jika
-                <span>Nominal Transaksi, </span>
-                dan <span>Nomor Virtual Account </span>
-                benar, pilih Benar
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                7
-              </div>
-              <div className="col-span-4">
-                Setelah berhasil saldo JakOnePay Kamu akan bertambah
-              </div>
-            </div>
+            {getBodyDescriptionTopUpWithATMBankLain()}
+
+            {getDescriptionTopUpWithATMBankLain()}
           </AccordionBody>
         </Accordion>
 
@@ -235,27 +127,27 @@ function AccordionTopUp() {
         >
           <AccordionHeader
             onClick={() => handleOpen(3)}
-            className={`border-b-0 transition-colors ${open === 3 ? "text-blue-500 hover:!text-blue-700" : ""
-              }`}
+            className={`border-b-0 transition-colors ${
+              open === 3 ? "text-blue-500 hover:!text-blue-700" : ""
+            }`}
           >
             <div className="grid grid-cols-6">
               <div className="...">
                 <img
                   className=""
-                  width={60}
+                  width={30}
                   src={IcoJakoneMobile}
                   alt="My Image"
                 />
               </div>
               <div className="col-span-4 ml-4">
-                <strong>JakOne Mobile</strong> <br />
-                <small>Tanpa biaya isi saldo dari JakOne Mobile</small>
+                {getDescriptionHeaderTopUpWithATMJakOneMobile()}
               </div>
               <div className="relative w-32 ...">
                 <div className="absolute inset-x-0 top-0 h-16 ...">
                   <img
                     className="rotate-90"
-                    width={60}
+                    width={30}
                     src={IconChevronRight}
                     alt="My Image"
                   />
@@ -265,72 +157,11 @@ function AccordionTopUp() {
           </AccordionHeader>
           <AccordionBody className="">
             <h1 className="text-center font-semibold mb-2">
-              <strong>TopUp JakOne Pay</strong>
+              {getHeaderPageHowToTopUpWithJakonePay()}
             </h1>
-            <p>
-              Sekarang kamu dapat mengisi JakonePay melalui seluruh mesin ATM Bank
-              BKI. Ikuti Langkah-langkah di bawah ini untuk melakukan TopUp
-              JakOnePay
-            </p>
-            <div className="grid grid-cols-6 my-4">
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                1
-              </div>
-              <div className="col-span-4">
-                Pilih Menu <span>Pembayaran</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                2
-              </div>
-              <div className="col-span-4">
-                Pilih Menu
-                <span>Virtual Account</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                3
-              </div>
-              <div className="col-span-4">
-                Masukan <span>No. Virtual Account (9980) </span>
-                yang diikuti dengan nomor Handphone Kamu.
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                4
-              </div>
-              <div className="col-span-4">
-                kosongkan
-                <span>Kode Pembayaran</span>
-                lalu pilih <span>Lanjut</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                5
-              </div>
-              <div className="col-span-4">
-                Masukan
-                <span>Nominal Transaksi, </span>
-                lalu pilih <span>Lanjut</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                6
-              </div>
-              <div className="col-span-4">
-                Jika
-                <span>Nominal Transaksi, </span>
-                dan <span>Nomor Virtual Account </span>
-                benar, pilih Benar
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                7
-              </div>
-              <div className="col-span-4">
-                Setelah berhasil saldo JakOnePay Kamu akan bertambah
-              </div>
-            </div>
+            {getBodyDescriptionTopUpWithATMDebitKredit()}
+
+            {getDescriptionTopUpWithATMDebitKredit()}
           </AccordionBody>
         </Accordion>
 
@@ -340,25 +171,22 @@ function AccordionTopUp() {
         >
           <AccordionHeader
             onClick={() => handleOpen(4)}
-            className={`border-b-0 transition-colors ${open === 4 ? "text-blue-500 hover:!text-blue-700" : ""
-              }`}
+            className={`border-b-0 transition-colors ${
+              open === 4 ? "text-blue-500 hover:!text-blue-700" : ""
+            }`}
           >
             <div className="grid grid-cols-6">
               <div className="...">
-                <img className="" width={60} src={IconCc2} alt="My Image" />
+                <img className="" width={30} src={IconCc2} alt="My Image" />
               </div>
               <div className="col-span-4 ml-4">
-                <strong>Kartu Debit/Kredit</strong> <br />
-                <small>
-                  Praktis dan aman isi saldo dengan Debit atau Kartu Kredit
-                  International
-                </small>
+                {getDescriptionHeaderTopUpWithATMDebitKredit()}
               </div>
               <div className="relative w-32 ...">
                 <div className="absolute inset-x-0 top-0 h-16 ...">
                   <img
                     className="rotate-90"
-                    width={60}
+                    width={30}
                     src={IconChevronRight}
                     alt="My Image"
                   />
@@ -368,76 +196,14 @@ function AccordionTopUp() {
           </AccordionHeader>
           <AccordionBody className="">
             <h1 className="text-center font-semibold mb-2">
-              <strong>TopUp JakOne Pay</strong>
+              {getHeaderPageHowToTopUpWithJakonePay()}
             </h1>
-            <p>
-              Sekarang kamu dapat mengisi JakonePay melalui seluruh mesin ATM Bank
-              BKI. Ikuti Langkah-langkah di bawah ini untuk melakukan TopUp
-              JakOnePay
-            </p>
-            <div className="grid grid-cols-6 my-4">
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                1
-              </div>
-              <div className="col-span-4">
-                Pilih Menu <span>Pembayaran</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                2
-              </div>
-              <div className="col-span-4">
-                Pilih Menu
-                <span>Virtual Account</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                3
-              </div>
-              <div className="col-span-4">
-                Masukan <span>No. Virtual Account (9980) </span>
-                yang diikuti dengan nomor Handphone Kamu.
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                4
-              </div>
-              <div className="col-span-4">
-                kosongkan
-                <span>Kode Pembayaran</span>
-                lalu pilih <span>Lanjut</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                5
-              </div>
-              <div className="col-span-4">
-                Masukan
-                <span>Nominal Transaksi, </span>
-                lalu pilih <span>Lanjut</span>
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                6
-              </div>
-              <div className="col-span-4">
-                Jika
-                <span>Nominal Transaksi, </span>
-                dan <span>Nomor Virtual Account </span>
-                benar, pilih Benar
-              </div>
-              <br />
-              <div className="w-8 h-8 bg-red-200 rounded flex items-center justify-center mb-2">
-                7
-              </div>
-              <div className="col-span-4">
-                Setelah berhasil saldo JakOnePay Kamu akan bertambah
-              </div>
-            </div>
+            {getBodyDescriptionTopUpWithATMJakOneMobile()}
+
+            {getDescriptionTopUpWithATMJakOneMobile()}
           </AccordionBody>
         </Accordion>
       </div>
-
     </div>
   );
 }
