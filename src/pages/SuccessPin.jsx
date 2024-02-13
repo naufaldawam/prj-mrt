ef186bc0051420061a5962183e7fe752c8e7a5e2
fadilahyudapratama <btufadilahyudapratama@gmail.com>
2024-02-13 16:03:50 +0700

    update pop succes pin

--- src/pages/SuccessPin.jsx
+++ src/pages/SuccessPin.jsx
@@ -1,50 +1,45 @@
-// CreatePin.js
 import React from "react";
-import { Link } from "react-router-dom";
-import ImageSucces from "../assets/icons/icon-success.png";
-import ImageSuccesPeople from "../assets/icons/success-image.png";
-
-const SuccessPin = () => {
-  const apiUrl = process.env.API_JAVA_URL;
-  console.log(apiUrl)
+import Lottie from "lottie-react";
+import AnimationSuccess from "./../components/lottieFiles/AnimationSuccess.json";
+import ImageSucces from "../assets/icons/logo_bank_dki.png";
+import { DescriptionSuccess } from "./../constantFile/I_Constant";
+const SuccesPageWithLottie = () => {
+  const handleClose = () => {
+    // Panggil fungsi onClose saat tombol ditutup
+    window.location.href = "/register/bdki";
+  };
   return (
-    <div>
-      <div className="max-w-md mx-auto my-2 bg-white rounded-md overflow-hidden shadow-sm">
-        <div className="p-4 items-center justify-center">
-          <div className="flex items-center justify-center my-4">
-            <img className="" width={60} src={ImageSucces} alt="My Image" />
-          </div>
-          <h6 className="text-xl my-2 text-center">CONGRATULATION</h6>
-          <strong>
-            <h2 className="text-xl font-semibold my-2 text-center">
-              Fadhilah Yuda Pratama
-            </h2>
-          </strong>
-          <div className="flex items-center justify-center my-4">
-            <img
-              className=""
-              width={200}
-              src={ImageSuccesPeople}
-              alt="My Image"
-            />
-          </div>
-          <p className="text-center">
-            <b>registration Successful !!!!</b>
-          </p>
-          <Link to="/">
-            <button
-              className="mt-6 block w-full select-none rounded-lg bg-red-700 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              type="button"
-              data-ripple-light="true"
-            >
-              let is go to your home page
-            </button>
-          </Link>
-        </div>
+    <div
+      style={{
+        marginTop: "2rem",
+        zIndex: 999,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div className="">
+        <img className="" width={200} src={ImageSucces} alt="My Image" />
       </div>
-      <div className="bg-gray-50"></div>
+      <div className="">
+        <Lottie
+          animationData={AnimationSuccess}
+          loop={true}
+          autoplay={true}
+          style={{
+            width: "150px",
+            height: "150px",
+          }}
+        />
+      </div>
+      <strong>
+        <h2 className="text-lg font-bold text-red-500 text-center">
+          Top Up JakOne
+        </h2>
+      </strong>
+      <p className="text-sm">AYI ABDUL AZIZ</p>
     </div>
   );
 };
-
-export default SuccessPin;
+export default SuccesPageWithLottie;
