1a3e9f6ec1f38c84be034eef2e1c261803e4500e
fadilahyudapratama <btufadilahyudapratama@gmail.com>
2024-02-13 12:02:11 +0700

    update feature top up

--- src/pages/accordionTopupPage/AccordionTopUp.jsx
+++ src/pages/accordionTopupPage/AccordionTopUp.jsx
@@ -4,7 +4,6 @@ import {
   AccordionHeader,
   AccordionBody,
 } from "@material-tailwind/react";
-import IconChevronRight from "../../assets/icons/ico_chevron_right.png";
 import IcoBankCard from "../../assets/icons/ico_bank_card.png";
 import IcoAccountBalance from "../../assets/icons/ico_account_balance.png";
 import IcoJakoneMobile from "../../assets/icons/ico_jakone_mobile.png";
@@ -26,186 +25,106 @@ import {
   getHeaderPageHowToTopUpWithJakonePay,
 } from "../../constantFile/I_Constant";
 
-function AccordionTopUp() {
+function Icon({ id, open }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}
+
+function AccordionCustomIcon() {
   const [open, setOpen] = React.useState(0);
+
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
   return (
-    <div>
-      <div className="text-2xl my-8 mx-4 text-center">
-        <h1>{getHeaderPageHowToTopUpWithJakonePay()}</h1>
-      </div>
-
-      <div className="p-7">
+    <>
+      <h2 className="mx-5 my-5 font-medium text-xl">
+        {getHeaderPageHowToTopUpWithJakonePay()}
+      </h2>
+      <div className="m-5">
         <Accordion
           open={open === 1}
-          className="mb-2  border border-pink-300 px-4"
+          icon={<Icon id={1} open={open} />}
+          className=""
         >
-          <AccordionHeader
-            onClick={() => handleOpen(1)}
-            className={`border-b-0 transition-colors ${
-              open === 1 ? "text-blue-500 hover:!text-blue-700" : ""
-            }`}
-          >
-            <div className="grid grid-cols-6">
-              <div className="...">
-                <img className="" width={30} src={IcoBankCard} alt="My Image" />
-              </div>
-              <div className="col-span-4 ml-4">
-                {getDescriptionHeaderTopUpWithATMBankDki()}
-              </div>
-              <div className="relative w-32 ...">
-                <div className="absolute inset-x-0 top-0 h-16 ...">
-                  <img
-                    className="rotate-90"
-                    width={30}
-                    src={IconChevronRight}
-                    alt="My Image"
-                  />
-                </div>
-              </div>
+          <AccordionHeader onClick={() => handleOpen(1)}>
+            <img className="" width={25} src={IcoBankCard} alt="My Image" />
+            <div className="text-base -ml-28">
+              {getDescriptionHeaderTopUpWithATMBankDki()}
             </div>
           </AccordionHeader>
-          <AccordionBody className="">
-            <h1 className="text-center font-semibold mb-2">
-              {getHeaderPageHowToTopUpWithJakonePay()}
-            </h1>
-
+          <AccordionBody>
             {getBodyDescriptionTopUpWithATMBankDki()}
 
             {getDescriptionTopUpWithATMBankDki()}
           </AccordionBody>
         </Accordion>
-
-        <Accordion
-          open={open === 2}
-          className="mb-2  border border-pink-300 px-4"
-        >
-          <AccordionHeader
-            onClick={() => handleOpen(2)}
-            className={`border-b-0 transition-colors ${
-              open === 2 ? "text-blue-500 hover:!text-blue-700" : ""
-            }`}
-          >
-            <div className="grid grid-cols-6">
-              <div className="...">
-                <img
-                  className=""
-                  width={30}
-                  src={IcoAccountBalance}
-                  alt="My Image"
-                />
-              </div>
-              <div className="col-span-4 ml-4">
-                {getDescriptionHeaderTopUpWithATMBankLain()}
-              </div>
-              <div className="relative w-32 ...">
-                <div className="absolute inset-x-0 top-0 h-16 ...">
-                  <img
-                    className="rotate-90"
-                    width={30}
-                    src={IconChevronRight}
-                    alt="My Image"
-                  />
-                </div>
-              </div>
+        <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
+          <AccordionHeader onClick={() => handleOpen(2)}>
+            <img
+              className="mr-5"
+              width={25}
+              src={IcoAccountBalance}
+              alt="My Image"
+            />
+            <div className="text-base">
+              {getDescriptionHeaderTopUpWithATMBankLain()}
             </div>
           </AccordionHeader>
-          <AccordionBody className="">
-            <h1 className="text-center font-semibold mb-2">
-              {getHeaderPageHowToTopUpWithJakonePay()}
-            </h1>
+          <AccordionBody>
             {getBodyDescriptionTopUpWithATMBankLain()}
 
             {getDescriptionTopUpWithATMBankLain()}
           </AccordionBody>
         </Accordion>
-
-        <Accordion
-          open={open === 3}
-          className="mb-2  border border-pink-300 px-4"
-        >
-          <AccordionHeader
-            onClick={() => handleOpen(3)}
-            className={`border-b-0 transition-colors ${
-              open === 3 ? "text-blue-500 hover:!text-blue-700" : ""
-            }`}
-          >
-            <div className="grid grid-cols-6">
-              <div className="...">
-                <img
-                  className=""
-                  width={30}
-                  src={IcoJakoneMobile}
-                  alt="My Image"
-                />
-              </div>
-              <div className="col-span-4 ml-4">
-                {getDescriptionHeaderTopUpWithATMJakOneMobile()}
-              </div>
-              <div className="relative w-32 ...">
-                <div className="absolute inset-x-0 top-0 h-16 ...">
-                  <img
-                    className="rotate-90"
-                    width={30}
-                    src={IconChevronRight}
-                    alt="My Image"
-                  />
-                </div>
-              </div>
+        <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
+          <AccordionHeader onClick={() => handleOpen(3)}>
+            <img
+              className="mr-5"
+              width={25}
+              src={IcoJakoneMobile}
+              alt="My Image"
+            />
+            <div className="text-base">
+              {getDescriptionHeaderTopUpWithATMJakOneMobile()}
             </div>
           </AccordionHeader>
-          <AccordionBody className="">
-            <h1 className="text-center font-semibold mb-2">
-              {getHeaderPageHowToTopUpWithJakonePay()}
-            </h1>
+          <AccordionBody>
             {getBodyDescriptionTopUpWithATMDebitKredit()}
 
             {getDescriptionTopUpWithATMDebitKredit()}
           </AccordionBody>
         </Accordion>
-
-        <Accordion
-          open={open === 4}
-          className="mb-2  border border-pink-300 px-4"
-        >
-          <AccordionHeader
-            onClick={() => handleOpen(4)}
-            className={`border-b-0 transition-colors ${
-              open === 4 ? "text-blue-500 hover:!text-blue-700" : ""
-            }`}
-          >
-            <div className="grid grid-cols-6">
-              <div className="...">
-                <img className="" width={30} src={IconCc2} alt="My Image" />
-              </div>
-              <div className="col-span-4 ml-4">
-                {getDescriptionHeaderTopUpWithATMDebitKredit()}
-              </div>
-              <div className="relative w-32 ...">
-                <div className="absolute inset-x-0 top-0 h-16 ...">
-                  <img
-                    className="rotate-90"
-                    width={30}
-                    src={IconChevronRight}
-                    alt="My Image"
-                  />
-                </div>
-              </div>
+        <Accordion open={open === 4} icon={<Icon id={4} open={open} />}>
+          <AccordionHeader onClick={() => handleOpen(4)}>
+            <img className="mr-5" width={25} src={IconCc2} alt="My Image" />
+            <div className="text-base">
+              {getDescriptionHeaderTopUpWithATMDebitKredit()}
             </div>
           </AccordionHeader>
-          <AccordionBody className="">
-            <h1 className="text-center font-semibold mb-2">
-              {getHeaderPageHowToTopUpWithJakonePay()}
-            </h1>
+          <AccordionBody>
             {getBodyDescriptionTopUpWithATMJakOneMobile()}
 
             {getDescriptionTopUpWithATMJakOneMobile()}
           </AccordionBody>
         </Accordion>
       </div>
-    </div>
+    </>
   );
 }
-
-export default AccordionTopUp;
+export default AccordionCustomIcon;
